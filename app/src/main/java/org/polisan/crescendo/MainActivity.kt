299c09ec47d9c62a067a268b274c94fddf40d8c3
Oleh Polisan <polisanoleg@gmail.com>
2023-07-08 00:11:03 +0300

    Added simple interface and connection to socket server

--- app/src/main/java/org/polisan/crescendo/MainActivity.kt
+++ app/src/main/java/org/polisan/crescendo/MainActivity.kt
@@ -1,43 +1,133 @@
 package org.polisan.crescendo
 
+import SocketConnection
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.KeyboardArrowDown
+import androidx.compose.material.icons.rounded.PlayArrow
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import org.polisan.crescendo.ui.theme.CrescendoTheme
 
-class MainActivity : ComponentActivity() {
+interface ConnectionListener {
+    fun onConnectionSuccess()
+    fun onConnectionLost()
+}
+class MainActivity : ComponentActivity(), ConnectionListener {
+    private val isConnected = mutableStateOf(false)
+    var connection: SocketConnection? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        connectToSocket()
         setContent {
             CrescendoTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    if (isConnected.value) {
+                        ButtonRow(this)
+                    } else {
+                        ConnectButton(onConnect = { connectToSocket() })
+                    }
                 }
             }
+        }
+
+    }
+
+    private fun connectToSocket() {
+        lifecycleScope.launch {
+            connection = SocketConnection("10.0.2.2", 4308, this@MainActivity)
+            isConnected.value = connection!!.isConnected()
+            Log.d("SOCKET", "isConnected ${isConnected.value}")
+        }
+    }
+
+    override fun onConnectionSuccess() {
+        isConnected.value = true
+    }
+
+    override fun onConnectionLost() {
+        isConnected.value = false
+    }
+}
+
+
+@SuppressLint("SuspiciousIndentation")
+@Composable
+fun ButtonRow(lifecycleOwner: LifecycleOwner) {
+    val mainActivity = remember(lifecycleOwner) {
+        lifecycleOwner as MainActivity
+    }
+    Row(Modifier.padding(20.dp)) {
+        IconButton(onClick = {
+            if(mainActivity.connection?.isConnected() == true)
+                //mainActivity.connection?.sendString("Previous")
+                mainActivity.connection?.sendByte(1)
+        }) {
+            Icon(
+                Icons.Rounded.ArrowBack,
+                contentDescription = "Previous"
+            )
+        }
+        Spacer(modifier = Modifier.width(2.dp))
+        IconButton(onClick = {
+            if(mainActivity.connection?.isConnected() == true)
+            //mainActivity.connection?.sendString("PlayPause")
+                mainActivity.connection?.sendByte(2)
+        }) {
+            Icon(
+                Icons.Rounded.PlayArrow,
+                contentDescription = "PlayPause"
+            )
+        }
+        Spacer(modifier = Modifier.width(2.dp))
+        IconButton(onClick = {
+            if(mainActivity.connection?.isConnected() == true)
+                //mainActivity.connection?.sendString("Next")
+                mainActivity.connection?.sendByte(3)
+        }) {
+            Icon(
+                Icons.Rounded.ArrowForward,
+                contentDescription = "Next"
+            )
+        }
+
+        Spacer(modifier = Modifier.width(2.dp))
+        IconButton(onClick = {
+            if(mainActivity.connection?.isConnected() == true)
+            //mainActivity.connection?.sendString("Get")
+                mainActivity.connection?.sendByte(4)
+        }) {
+            Icon(
+                Icons.Rounded.KeyboardArrowDown,
+                contentDescription = "Get"
+            )
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CrescendoTheme {
-        Greeting("Android")
+fun ConnectButton(onConnect: () -> Unit) {
+    Button(onClick = onConnect) {
+        Text(text = "Connect")
     }
 }
---
+++ app/src/main/java/org/polisan/crescendo/SocketConnection.kt
@@ -0,0 +1,173 @@
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.polisan.crescendo.ConnectionListener
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.net.ConnectException
+import java.net.InetSocketAddress
+import java.net.Socket
+import java.net.SocketException
+
+class SocketConnection(
+    private val ipAddress: String,
+    private val port: Int,
+    private val connectionListener: ConnectionListener?
+) {
+    private val socket: Socket = Socket()
+    private var reader: BufferedReader? = null
+
+    private var isConnectionClosed = false
+    private var serverAlive = true
+
+    init {
+        GlobalScope.launch(Dispatchers.IO) {
+
+            connect()
+        }
+    }
+
+    private suspend fun connect() {
+        try {
+            try {
+                withContext(Dispatchers.IO) {
+                    socket.connect(InetSocketAddress(ipAddress, port), 1000)
+                }
+                reader = BufferedReader(InputStreamReader(withContext(Dispatchers.IO) {
+                    socket.getInputStream()
+                }))
+
+                Log.d("SOCKET", "Connected to $ipAddress:$port")
+                isConnectionClosed = false
+
+            } catch(e: ConnectException) {
+                Log.d("SOCKET", "Can't connect to $ipAddress:$port")
+                isConnectionClosed = true
+                return
+            }
+
+            // Notify the connection success
+            connectionListener?.onConnectionSuccess()
+
+            // Launch the connection checker coroutine
+            GlobalScope.launch(Dispatchers.IO) {
+                while (!socket.isClosed) {
+                    if (socket.isClosed || !socket.isConnected) {
+                        Log.e("SOCKET", "Connection to $ipAddress:$port lost.")
+                        withContext(Dispatchers.Main) {
+                            connectionListener?.onConnectionLost()
+                        }
+                        break
+                    }
+
+                    try {
+                        sendByte(0)
+                    } catch (e: Exception) {
+                        Log.e("SOCKET", "Exception: ${e.message}")
+                        e.printStackTrace()
+                    }
+
+                    delay(1000)
+                }
+            }
+// Launch the response waiting coroutine
+            GlobalScope.launch(Dispatchers.IO) {
+                val buffer = CharArray(1024)
+                val responseBuilder = StringBuilder()
+
+                while (!socket.isClosed) {
+                    try {
+
+                        if (reader?.ready() == true) {
+                            val bytesRead = reader?.read(buffer)
+                            if (bytesRead != null && bytesRead != -1) {
+                                val receivedData = buffer.sliceArray(0 until bytesRead)
+                                responseBuilder.append(receivedData)
+                            }
+
+                            val response = responseBuilder.toString()
+                            if (response.isNotEmpty()) {
+                                Log.d("SOCKET", "Received response: $response")
+                                // Perform any desired processing with the response
+
+                                // Reset the response builder
+                                responseBuilder.clear()
+                            }
+                        }
+                    } catch (e: Exception) {
+                        Log.e("SOCKET", "Exception: ${e.message}")
+                        e.printStackTrace()
+                    }
+
+                    delay(100)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun sendString(message: String) {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val dOut = DataOutputStream(socket.getOutputStream())
+                Log.d("SOCKET", "Sending message $message")
+                dOut.writeUTF(message)
+                dOut.flush()
+            } catch (e: SocketException) {
+                Log.e("SOCKET", "Exception: ${e.message}")
+                e.printStackTrace()
+                socket.close()
+                connectionListener?.onConnectionLost()
+            }
+        }
+    }
+
+    fun sendByte(int: Int) {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val dOut = DataOutputStream(socket.getOutputStream())
+                if(int != 0)
+                  Log.d("SOCKET", "Sending int $int")
+                dOut.writeByte(int)
+                dOut.flush()
+            } catch (e: SocketException) {
+                Log.e("SOCKET", "Exception: ${e.message}")
+                e.printStackTrace()
+                socket.close()
+                connectionListener?.onConnectionLost()
+            }
+        }
+    }
+
+    fun receive(): String? {
+        return runCatching {
+            reader?.readLine()
+        }.getOrElse {
+            null
+        }
+    }
+
+    fun disconnect() {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                reader?.close()
+                socket.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun isConnected(): Boolean {
+        return socket.isConnected
+    }
+
+    fun isServerAlive(): Boolean {
+        return serverAlive
+    }
+}
